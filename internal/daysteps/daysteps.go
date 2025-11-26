6984d4eb01f5d48dffc602c21caf8a1e07cfdebb
Ilya Levin <levin1424@gmail.com>
2025-11-26 22:09:03 +0300

    second commit

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,7 +1,9 @@
 package daysteps
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 	"time"
@@ -18,42 +20,53 @@ const (
 
 func parsePackage(data string) (int, time.Duration, error) {
 	stepTime := strings.Split(data, ",")
+
 	if len(stepTime) != 2 {
-		return 0, 0, fmt.Errorf("ожидается два значения, которые разделены запятой")
+		return 0, 0, errors.New("data parsing error")
 	}
 
-	count, err := strconv.Atoi(strings.TrimSpace(stepTime[0]))
+	//count, err := strconv.Atoi(strings.TrimSpace(stepTime[0]))
+	count, err := strconv.Atoi(stepTime[0])
+
 	if err != nil {
-		return 0, 0, fmt.Errorf("не получилось прочитать количество шагов")
+		return 0, 0, errors.New("couldn't parse the number of steps")
 	}
 
 	if count <= 0 {
-		return 0, 0, fmt.Errorf("количество шагов должно быть больше нуля")
+		return 0, 0, errors.New("the number of steps must be greater than zero")
 	}
 
-	duration, err := time.ParseDuration(strings.TrimSpace(stepTime[1]))
+	//duration, err := time.ParseDuration(strings.TrimSpace(stepTime[1]))
+	duration, err := time.ParseDuration(stepTime[1])
+
 	if err != nil {
-		return 0, 0, fmt.Errorf("не получилось прочитать время")
+		return 0, 0, errors.New("couldn't parse the time")
+	}
+
+	if duration <= time.Duration(0) {
+		return 0, 0, errors.New("duration activity is too short")
 	}
 
 	return count, duration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
-	count, duration, err := parsePackage(data)
-	if err != nil {
-		fmt.Println(err)
-		return ""
-	}
 
-	if count <= 0 {
-		fmt.Println(err)
+	count, duration, err := parsePackage(data)
+
+	if err != nil {
+		log.Print(err)
 		return ""
 	}
 
 	metricWalk := float64(count) * stepLength
-	kilometrWalk := metricWalk / mInKm
+	kmWalk := metricWalk / mInKm
 	calories, err := spentcalories.WalkingSpentCalories(count, weight, height, duration)
 
-	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.", count, kilometrWalk, calories)
+	if err != nil {
+		log.Print(err)
+		return ""
+	}
+
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", count, kmWalk, calories)
 }
--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -1,6 +1,7 @@
 package spentcalories
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"strconv"
@@ -18,28 +19,40 @@ const (
 )
 
 func parseTraining(data string) (int, string, time.Duration, error) {
+
 	countTrainTime := strings.Split(data, ",")
+
 	if len(countTrainTime) != 3 {
-		return 0, "", 0, fmt.Errorf("длина слайса не совпадает")
+		err := errors.New("data parsing error")
+		return 0, "", 0, err
 	}
 
-	count, err := strconv.Atoi(strings.TrimSpace(countTrainTime[0]))
+	//count, err := strconv.Atoi(strings.TrimSpace(countTrainTime[0]))
+	count, err := strconv.Atoi(countTrainTime[0])
+
 	if err != nil {
-		return 0, "", 0, fmt.Errorf("не удалось преобразовать количество шагов")
+		err := errors.New("couldn't convert steps")
+		return 0, "", 0, err
 	}
 
 	if count <= 0 {
-		return 0, "", 0, fmt.Errorf("количество шагов должно быть больше нуля")
+		err := errors.New("the number of steps must be greater than zero")
+		return 0, "", 0, err
 	}
 
-	trainingType := countTrainTime[1]
+	//duration, err := time.ParseDuration(strings.TrimSpace(countTrainTime[2]))
+	duration, err := time.ParseDuration(countTrainTime[2])
 
-	duration, err := time.ParseDuration(strings.TrimSpace(countTrainTime[2]))
 	if err != nil {
-		return 0, "", 0, fmt.Errorf("не удалось преобразовать время")
+		return 0, "", 0, err
 	}
 
-	return count, trainingType, duration, nil
+	if duration <= time.Duration(0) {
+		err := errors.New("activity is too short")
+		return 0, "", 0, err
+	}
+
+	return count, countTrainTime[1], duration, nil
 }
 
 func distance(steps int, height float64) float64 {
@@ -49,7 +62,7 @@ func distance(steps int, height float64) float64 {
 }
 
 func meanSpeed(steps int, height float64, duration time.Duration) float64 {
-	if duration <= 0 {
+	if duration <= time.Duration(0) {
 		return 0.0
 	}
 
@@ -57,7 +70,6 @@ func meanSpeed(steps int, height float64, duration time.Duration) float64 {
 }
 
 func TrainingInfo(data string, weight, height float64) (string, error) {
-	var calories float64
 
 	count, typeTrain, duration, err := parseTraining(data)
 	if err != nil {
@@ -66,57 +78,97 @@ func TrainingInfo(data string, weight, height float64) (string, error) {
 	}
 
 	if height <= 0 || weight <= 0 {
-		fmt.Errorf("рост и вес должны быть положительными")
+		err := errors.New("height and weight should be positive")
 		log.Println(err)
 		return "", err
 	}
 
-	dist := distance(count, height)
-	speed := meanSpeed(count, height, duration)
+	//dist := distance(count, height)
+	//speed := meanSpeed(count, height, duration)
 
 	switch typeTrain {
 	case "Ходьба":
-		c, err := WalkingSpentCalories(count, weight, height, duration)
+
+		dist := distance(count, height)
+
+		averageSpeed := meanSpeed(count, height, duration)
+
+		calorise, err := WalkingSpentCalories(count, weight, height, duration)
+
 		if err != nil {
-			log.Println(err)
 			return "", err
 		}
-		calories = c
+
+		result := fmt.Sprintf("Тип тренировки: Ходьба\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n",
+			duration.Hours(), dist, averageSpeed, calorise)
+
+		return result, nil
 	case "Бег":
-		c, err := RunningSpentCalories(count, weight, height, duration)
+
+		dist := distance(count, height)
+
+		averageSpeed := meanSpeed(count, height, duration)
+
+		calorise, err := RunningSpentCalories(count, weight, height, duration)
+
 		if err != nil {
-			log.Println(err)
 			return "", err
 		}
-		calories = c
+
+		result := fmt.Sprintf("Тип тренировки: Бег\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n",
+			duration.Hours(), dist, averageSpeed, calorise)
+
+		return result, nil
 	default:
-		err := fmt.Errorf("неизвестный тип тренировки")
-		log.Println(err)
+
+		err := errors.New("неизвестный тип тренировки")
 		return "", err
 	}
-
-	result := fmt.Sprintf(
-		"Количество шагов: %d.\nДистанция составила %.2f км.\nСредняя скорость: %.2f км/ч.\nВы сожгли %.2f ккал.",
-		count,
-		dist,
-		speed,
-		calories,
-	)
-
-	return result, nil
 }
 
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	if steps == 0 || duration == 0 || weight == 0.0 || height == 0.0 {
-		return 0.0, fmt.Errorf("невозможно выполнить рассчет калорий при беге")
+	if steps <= 0 {
+		err := errors.New("incorrect number of steps when counting calories while running")
+		return 0.0, err
+	}
+
+	if duration <= time.Duration(0) {
+		err := errors.New("incorrect time when counting calories while running")
+		return 0.0, err
+	}
+
+	if weight <= 0.0 {
+		err := errors.New("incorrect weight when counting calories while running")
+		return 0.0, err
+	}
+
+	if height <= 0.0 {
+		err := errors.New("incorrect height when counting calories while running")
+		return 0.0, err
 	}
 
 	return (meanSpeed(steps, height, duration) * weight * duration.Minutes()) / minInH, nil
 }
 
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	if steps == 0 || duration == 0 || weight == 0.0 || height == 0.0 {
-		return 0.0, fmt.Errorf("невозможно выполнить рассчет калорий при ходьбе")
+	if steps <= 0 {
+		err := errors.New("incorrect number of steps when counting calories while walking")
+		return 0.0, err
+	}
+
+	if duration <= time.Duration(0) {
+		err := errors.New("incorrect time when counting calories while walking")
+		return 0.0, err
+	}
+
+	if weight <= 0.0 {
+		err := errors.New("incorrect weight when counting calories while walking")
+		return 0.0, err
+	}
+
+	if height <= 0.0 {
+		err := errors.New("incorrect height when counting calories while walking")
+		return 0.0, err
 	}
 
 	calories := (meanSpeed(steps, height, duration) * weight * duration.Minutes()) / minInH
